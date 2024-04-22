d948c02405a625dff73ef2802e40a62e2577fac4
Hirantha Ranathunga <hirantharanathunga13@gmail.com>
2024-04-23 00:21:58 +0530

    initial commit

---
+++ static/script.js
@@ -0,0 +1,7 @@
+function togglePopup(jobTitle){
+  document.getElementById("popup-" + jobTitle).classList.toggle("active");
+}
+
+function togglePopupforCourse(courseTitle){
+  document.getElementById("popup-" + courseTitle).classList.toggle("active");
+}
